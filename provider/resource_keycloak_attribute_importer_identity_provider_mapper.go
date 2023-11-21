480330ac9db310845455956348699468037db93c
ktogias <ktogias@cti.gr>
2023-11-21 14:00:32 +0200

    Add gsis attribute importer

--- provider/resource_keycloak_attribute_importer_identity_provider_mapper.go
+++ provider/resource_keycloak_attribute_importer_identity_provider_mapper.go
@@ -67,6 +67,10 @@ func getAttributeImporterIdentityProviderMapperFromData(ctx context.Context, dat
 		}
 
 		rec.Config.Claim = data.Get("claim_name").(string)
+	} else if identityProvider.ProviderId == "gsis-taxis-test" {
+		rec.IdentityProviderMapper = "gsis-user-attribute-mapper"
+		rec.Config.JsonField = data.Get("claim_name").(string)
+		rec.Config.UserAttributeName = data.Get("user_attribute").(string)
 	} else if identityProvider.ProviderId == "facebook" || identityProvider.ProviderId == "google" {
 		rec.IdentityProviderMapper = fmt.Sprintf("%s-user-attribute-mapper", identityProvider.ProviderId)
 		rec.Config.JsonField = data.Get("claim_name").(string)
